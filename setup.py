3c0b3b76c7cf6980b1585fd1923089d99f57b4e0
AboorvaDevarajan <aburvadevarajan@gmail.com>
2015-10-15 02:57:40 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='vmalloc',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      author='Aboorva Devarajan',
+      author_email='aburvadevarajan@gmail.com',
       url='https://www.python.org/community/sigs/current/distutils-sig',
-      install_requires=['Flask>=0.7.2', 'MarkupSafe'],
+      install_requires=['flask','numpy','matplotlib','pyparsing'],
      )
